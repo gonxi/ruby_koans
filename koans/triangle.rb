003356a341dc69ca5ca87fb0118794532fc361f5
gonxi <gonxi.gonzalo@gmail.com>
2015-02-16 18:52:32 +0100

    Koan 156

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+ if (a == b) and (a == C)
+ 	return :equilateral
+ end
+
 end
 
 # Error class used in part 2.  No need to change this code.
